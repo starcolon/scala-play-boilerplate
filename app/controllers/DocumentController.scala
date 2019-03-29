9d3e87ecc531751a5149e053ec22aca2e3f045d9
Stefan Sprenger <info@stefan-sprenger.com>
2019-03-29 10:51:05 +0100

    Cleanup class definition

--- app/controllers/DocumentController.scala
+++ app/controllers/DocumentController.scala
@@ -6,11 +6,7 @@ import play.api._
 import play.api.libs.json.Json
 import play.api.mvc._
 
-class DocumentController @Inject()(
-  controllerComponents: ControllerComponents
-)(
-) extends AbstractController(controllerComponents) {
-
+class DocumentController @Inject()(cc: ControllerComponents)() extends AbstractController(cc) {
   def index = Action {
     Ok(Json.obj("Message" -> "Please implement me!"))
   }
