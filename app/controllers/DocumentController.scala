eb6ec25e9ea9f52e9d1ec8b94652b2cadeda6628
Pathompong Ruangyam <pataoengineer@gmail.com>
2019-04-01 13:55:28 +0200

    boilerplate routes

--- app/controllers/DocumentController.scala
+++ app/controllers/DocumentController.scala
@@ -6,8 +6,30 @@ import play.api._
 import play.api.libs.json.Json
 import play.api.mvc._
 
-class DocumentController @Inject()(cc: ControllerComponents)() extends AbstractController(cc) {
+class DocumentController @Inject()
+(cc: ControllerComponents)() extends AbstractController(cc) {
+  // Root 
   def index = Action {
     Ok(Json.obj("Message" -> "Please implement me!"))
+  }
+
+  def listDocuments() = Action {
+    Ok(Json.obj("Message" -> "Please input something"))
+  }
+
+  def getDocument(id: String) = Action {
+    Ok(Json.obj("Message" -> "Please input something"))
+  }
+
+  def createDocument() = Action {
+    Ok(Json.obj("Message" -> "Please input something"))
+  }
+
+  def updateDocument(id: String) = Action {
+    Ok(Json.obj("Message" -> "Please input something"))
+  }
+
+  def deleteDocument(id: String) = Action {
+    Ok(Json.obj("Message" -> "Please input something"))
   }
 }
